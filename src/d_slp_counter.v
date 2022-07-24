cfd67dfb59cfe7be36f380ad80d94dfbaccfa1b7
CarlosCraveiro <carlos.craveiro@usp.br>
2022-07-24 19:18:05 -0300

    Adiciona API para contador presente no ADC de rampa dupla

---
+++ src/d_slp_counter.v
@@ -0,0 +1,12 @@
+`timescale 1 ns/100 ps
+
+module d_slp_counter( input enable,
+                      input reset,
+                      input load,
+                      output [0:6] ssg_1,
+                      output [0:6] ssg_2,
+                      output [0:6] ssg_3, 
+                      output en_machine);
+// Implementar funcionamento do contador presente no ADC de Rampa Dupla
+endmodule
+
