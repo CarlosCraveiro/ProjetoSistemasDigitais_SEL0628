6e9165f8a1f1f354312d09e4ab510ef320980d3d
CarlosCraveiro <carlos.craveiro@usp.br>
2022-07-24 18:57:48 -0300

    Adiciona API para modulo contador BCD

---
+++ src/counter_bcd.v
@@ -0,0 +1,9 @@
+`timescale 1 ns/100 ps
+
+module counter_bcd(  input clk,
+                        input enable,
+                        input reset,
+                        output reg [0:3] bcd,
+                        output cnt_9);
+// Implementar funcionamento do modulo contador BCD
+endmodule
