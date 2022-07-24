df97212898f89c8491efc2f607fbc42e2e4facb2
CarlosCraveiro <carlos.craveiro@usp.br>
2022-07-24 18:45:11 -0300

    Adiciona API para registrador de 4-bits

---
+++ src/reg_4bit.v
@@ -0,0 +1,8 @@
+`timescale 1 ns/100 ps
+
+module reg_4bit ( input ld,
+                  input clk,
+                  input [0:3] d,
+                  output [0:3] q);
+// Inserir codigo para funcionamento do registrador de 4 bits.
+endmodule
