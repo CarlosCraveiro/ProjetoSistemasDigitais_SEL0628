692546ea9fdee507a904368642e0e801c09a5009
CarlosCraveiro <carlos.craveiro@usp.br>
2022-07-24 18:50:59 -0300

    Adiciona API para conversor BCD para sete-segmentos

---
+++ src/bcd_to_ssg.v
@@ -0,0 +1,6 @@
+`timescale 1 ns/100 ps
+
+module bcd_to_ssg ( input [0:3] bcd,
+                    output [0:6] ssg);
+// Inserir codigo de funcionamento para conversor bcd para sete segmentos
+endmodule
