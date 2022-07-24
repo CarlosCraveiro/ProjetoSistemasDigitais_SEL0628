2cb252881c0ad3779c18a7e60ea94aca561b785d
CarlosCraveiro <carlos.craveiro@usp.br>
2022-07-24 19:17:44 -0300

    Adiciona API para modulo de contagem

---
+++ src/counter_block.v
@@ -0,0 +1,10 @@
+`timescale 1 ns/100 ps
+
+module counter_block( input enable,
+                      input reset,
+                      input clk,
+                      input load,
+                      output [0:6] ssg,
+                      output enable_next);
+// Implementar logica de funcionamento do bloco de contagem
+endmodule
