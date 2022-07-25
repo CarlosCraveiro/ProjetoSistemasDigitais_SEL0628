cab533dd71e10107a35fd28e39d4da627c24242c
CarlosCraveiro <carlos.craveiro@usp.br>
2022-07-25 10:38:52 -0300

    Implementa API para maquina de controle do ADC commit fecha issue #7

---
+++ src/control_machine.v
@@ -0,0 +1,13 @@
+`timescale 1 ns/100 ps
+
+module control_machine( input start,
+                        input clk,
+                        input finished_counting,
+                        input cap_discharged,
+                        output ch_Vmeasured,
+                        output ch_Vref,
+                        output ch_Zero,
+                        output reset,
+                        output enable_counting);
+  // Implementar logica de controle de ADC do tipo rampa dupla
+endmodule
