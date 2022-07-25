00b042f9f9ebcac767c77eda419ac164f6f05063
CarlosCraveiro <carlos.craveiro@usp.br>
2022-07-24 23:47:07 -0300

    Implementa bloco de contagem

--- src/counter_block.v
+++ src/counter_block.v
@@ -1,4 +1,7 @@
 `timescale 1 ns/100 ps
+`include "bcd_to_ssg.v"
+`include "reg_4bit.v"
+`include "counter_bcd.v"
 
 module counter_block( input enable,
                       input reset,
@@ -6,5 +9,9 @@ module counter_block( input enable,
                       input load,
                       output [6:0] ssg,
                       output enable_next);
-// Implementar logica de funcionamento do bloco de contagem
+  wire [3:0] bcd_in;
+  wire [3:0] bcd_out;
+  counter_bcd cntr_bcd(clk, enable, reset, bcd_in, enable_next);
+  reg_4bit r4b(load, clk, bcd_in, bcd_out);
+  bcd_to_ssg bcd2ssg(bcd_out, ssg);  
 endmodule
