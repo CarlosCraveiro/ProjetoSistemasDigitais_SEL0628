6afe13717bce514fc2943f3588c0b1e8eec48c05
CarlosCraveiro <carlos.craveiro@usp.br>
2022-07-26 02:05:38 -0300

    Corrige ativamento precoce do enable machine (de 900 para 999)

--- src/d_slp_counter.v
+++ src/d_slp_counter.v
@@ -9,9 +9,10 @@ module d_slp_counter( input enable,
                       output [6:0] ssg_1,
                       output [6:0] ssg_2, 
                       output en_machine);
-  wire en_1, en_2, en_3;
+  wire en_1, en_2, en_3, en_4;
   counter_block b0(enable, reset, clk, load, ssg_0, en_1);
   counter_block b1(en_1, reset, clk, load, ssg_1, en_2);
   and(en_3, en_2, en_1);
-  counter_block b2(en_3, reset, clk, load, ssg_2, en_machine);
+  counter_block b2(en_3, reset, clk, load, ssg_2, en_4);
+  and(en_machine, en_4, en_3);
 endmodule
